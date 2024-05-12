d840f710007eea2ab831df1ef69c14f1089a8f32
Željko Margeta <zeljko.margeta@gmail.com>
2024-05-12 23:50:20 +0200

    style: adjust terminal
    Force VI cursor in the shell.
    Disable italic font rendering (render normal style instead)

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,12 +1,21 @@
 local wezterm = require('wezterm')
 return {
   color_scheme = 'GruvboxDark',
-  default_cursor_style = 'BlinkingBar',
+  default_cursor_style = 'SteadyUnderline',
   font = wezterm.font_with_fallback({
-    { family = 'JuliaMono',      weight = 'Light',  italic = false, assume_emoji_presentation = false },
+    { family = 'JuliaMono',      weight = 'Light', assume_emoji_presentation = false },
     { family = 'JetBrains Mono', weight = 'Regular' }
   }),
   font_size = 12,
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font_with_fallback({
+        { family = 'JuliaMono',      weight = 'Light',   style = 'Normal', assume_emoji_presentation = false },
+        { family = 'JetBrains Mono', weight = 'Regular', style = 'Normal' }
+      })
+    }
+  },
   keys = {
     { key = 'LeftArrow',  mods = 'OPT|CMD',    action = wezterm.action.ActivateTabRelative(-1) },
     { key = 'RightArrow', mods = 'OPT|CMD',    action = wezterm.action.ActivateTabRelative(1) },
@@ -16,6 +25,7 @@ return {
   line_height = 1.1,
   send_composed_key_when_left_alt_is_pressed = true,
   send_composed_key_when_right_alt_is_pressed = false,
+  window_decorations = 'RESIZE',
   window_frame = {
     active_titlebar_bg = '#282828',
     active_titlebar_border_bottom = '#282828',
