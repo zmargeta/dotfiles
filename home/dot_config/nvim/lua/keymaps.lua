55d2d6afc8ae14b29a0c7aa214df55d199f0b35a
Željko Margeta <zeljko.margeta@gmail.com>
2024-06-04 22:57:50 +0200

    feat: add vim keymaps to nvim and ideavim

--- home/dot_config/nvim/lua/keymaps.lua
+++ home/dot_config/nvim/lua/keymaps.lua
@@ -1,7 +1,5 @@
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<S-F2>", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "<F2>", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", { desc = "Open [E]xplorer window" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
@@ -11,6 +9,9 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "Y", "y$")
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
