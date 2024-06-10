01d3ae1df2d22245f3efcc5cca91fe9ce268379c
Željko Margeta <zeljko.margeta@gmail.com>
2024-06-10 22:31:08 +0200

    chore: cleanup and formatting
    Remove asdf configuration and fix wezterm config formatting.

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,59 +1,59 @@
 local wezterm = require("wezterm")
 return {
-	color_scheme = "tokyonight_storm",
-	default_cursor_style = "BlinkingUnderline",
-	font = wezterm.font_with_fallback({
-		{ family = "JuliaMono", weight = "Light", assume_emoji_presentation = false },
-		{ family = "JetBrains Mono", weight = "Regular" },
-	}),
-	font_size = 12,
-	font_rules = {
-		{
-			italic = true,
-			font = wezterm.font_with_fallback({
-				{ family = "JuliaMono", weight = "Light", style = "Normal", assume_emoji_presentation = false },
-				{ family = "JetBrains Mono", weight = "Regular", style = "Normal" },
-			}),
-		},
-	},
-	front_end = "WebGpu",
-	hide_tab_bar_if_only_one_tab = true,
-	keys = {
-		{ key = "LeftArrow", mods = "OPT|CMD", action = wezterm.action.ActivateTabRelative(-1) },
-		{ key = "RightArrow", mods = "OPT|CMD", action = wezterm.action.ActivateTabRelative(1) },
-		{ key = "LeftArrow", mods = "SHIFT|OPT|CMD", action = wezterm.action.MoveTabRelative(-1) },
-		{ key = "RightArrow", mods = "SHIFT|OPT|CMD", action = wezterm.action.MoveTabRelative(1) },
-	},
-	line_height = 1.1,
-	colors = {
-		tab_bar = {
-			active_tab = {
-				bg_color = "#393d4e",
-				fg_color = "#a9b1d6",
-			},
-			inactive_tab = {
-				bg_color = "#24283b",
-				fg_color = "#959cbd",
-			},
-			new_tab = {
-				bg_color = "#24283b",
-				fg_color = "#959cbd",
-			},
-			inactive_tab_edge = "#24283b",
-		},
-	},
-	send_composed_key_when_left_alt_is_pressed = true,
-	send_composed_key_when_right_alt_is_pressed = false,
-	window_decorations = "RESIZE",
-	window_frame = {
-		active_titlebar_bg = "#24283b",
-		font = wezterm.font("Lekton"),
-		font_size = 13,
-	},
-	window_padding = {
-		left = "1cell",
-		right = "1cell",
-		top = "0.5cell",
-		bottom = "0cell",
-	},
+  color_scheme = "tokyonight_storm",
+  default_cursor_style = "BlinkingUnderline",
+  font = wezterm.font_with_fallback({
+    { family = "JuliaMono", weight = "Light", assume_emoji_presentation = false },
+    { family = "JetBrains Mono", weight = "Regular" },
+  }),
+  font_size = 12,
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font_with_fallback({
+        { family = "JuliaMono", weight = "Light", style = "Normal", assume_emoji_presentation = false },
+        { family = "JetBrains Mono", weight = "Regular", style = "Normal" },
+      }),
+    },
+  },
+  front_end = "WebGpu",
+  hide_tab_bar_if_only_one_tab = true,
+  keys = {
+    { key = "LeftArrow", mods = "OPT|CMD", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = "RightArrow", mods = "OPT|CMD", action = wezterm.action.ActivateTabRelative(1) },
+    { key = "LeftArrow", mods = "SHIFT|OPT|CMD", action = wezterm.action.MoveTabRelative(-1) },
+    { key = "RightArrow", mods = "SHIFT|OPT|CMD", action = wezterm.action.MoveTabRelative(1) },
+  },
+  line_height = 1.1,
+  colors = {
+    tab_bar = {
+      active_tab = {
+        bg_color = "#393d4e",
+        fg_color = "#a9b1d6",
+      },
+      inactive_tab = {
+        bg_color = "#24283b",
+        fg_color = "#959cbd",
+      },
+      new_tab = {
+        bg_color = "#24283b",
+        fg_color = "#959cbd",
+      },
+      inactive_tab_edge = "#24283b",
+    },
+  },
+  send_composed_key_when_left_alt_is_pressed = true,
+  send_composed_key_when_right_alt_is_pressed = false,
+  window_decorations = "RESIZE",
+  window_frame = {
+    active_titlebar_bg = "#24283b",
+    font = wezterm.font("Lekton"),
+    font_size = 13,
+  },
+  window_padding = {
+    left = "1cell",
+    right = "1cell",
+    top = "0.5cell",
+    bottom = "0cell",
+  },
 }
