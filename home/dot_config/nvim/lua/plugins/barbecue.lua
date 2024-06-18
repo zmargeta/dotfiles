3bb388d075d06f449bfe6dec8972797485a80238
Željko Margeta <zeljko.margeta@gmail.com>
2024-06-18 22:15:42 +0200

    feat: add barbecue nvim plugin

---
+++ home/dot_config/nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "utilyre/barbecue.nvim",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      theme = "tokyonight",
+    },
+  },
+}
