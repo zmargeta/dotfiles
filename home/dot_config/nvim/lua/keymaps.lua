f3d6230e732b7ea185af1b26f16b2adac744547a
Željko Margeta <zeljko.margeta@gmail.com>
2024-09-27 22:02:33 +0200

    fix: remove extra keymaps and formatting

--- home/dot_config/nvim/lua/keymaps.lua
+++ home/dot_config/nvim/lua/keymaps.lua
@@ -5,19 +5,15 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to the upper window" })
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Go to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Go to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Go to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Go to the upper window" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll upwards" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll downwards" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "Repeat /?" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Repeat /? opposite direction" })
 vim.keymap.set("n", "Y", "y$", { desc = "Yank end of line" })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("zmargeta", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("zmargeta", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
