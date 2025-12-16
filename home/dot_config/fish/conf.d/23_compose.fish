c0ecb9407a969898b91ca776fe31fec2e0b642fb
Željko Margeta <zeljko.margeta@gmail.com>
2025-12-16 21:24:02 +0100

    feat: switch docker to orbstack

--- home/dot_config/fish/conf.d/23_compose.fish
+++
@@ -1,7 +0,0 @@
-if type -q docker-compose
-  if type -q brew
-    mkdir -p $HOME/.docker/cli-plugins
-    ln -sfn (brew --prefix docker-compose)/bin/docker-compose $HOME/.docker/cli-plugins/docker-compose
-  end
-end
-
--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -43,3 +43,7 @@ if status is-interactive
   test -f $HOME/.config/fish/aliases.fish
   and source $HOME/.config/fish/aliases.fish
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
